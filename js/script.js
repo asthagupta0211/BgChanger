aef8589e6767b4d7009a12a9738b15396ba0fa11
Astha Gupta <astha.the.don@gmail.com>
2026-04-20 13:50:00 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function setColor(color) {
+    document.body.style.background = color;
+}
+
+function setGradient() {
+    document.body.style.background =
+        "linear-gradient(135deg, #ff7e5f, #feb47b)";
+}
+
+function setImage() {
+    document.body.style.background =
+        "url('https://picsum.photos/1600/900') no-repeat center center/cover";
+}
+
+function resetBg() {
+    document.body.style.background =
+        "linear-gradient(135deg, #667eea, #764ba2)";
+}
