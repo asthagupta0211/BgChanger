8ef52a09738bdc5d1293835db02b7b5d1d21cf74
Astha Gupta <astha.the.don@gmail.com>
2026-04-20 13:54:15 +0530

    update

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function setGradient() {
 
 function setImage() {
     document.body.style.background =
-        "url('https://picsum.photos/1600/900') no-repeat center center/cover";
+        "url(https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhdHxlbnwwfHwwfHx8MA%3D%3D') no-repeat center center/cover";
 }
 
 function resetBg() {
